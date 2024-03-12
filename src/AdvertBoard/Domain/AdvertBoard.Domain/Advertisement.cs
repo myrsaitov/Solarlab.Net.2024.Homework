8d0e9730220c3f077dc405ed49a470cbe819db89
Konstantin Myrsaitov <myrsaitov@gmail.com>
2024-03-12 18:16:20 +0300

    Добавлен Домен

---
+++ src/AdvertBoard/Domain/AdvertBoard.Domain/Advertisement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdvertBoard.Domain
+{
+    public class Advertisement
+    {
+        /// <summary>
+        /// Объявление
+        /// </summary>
+        public int Id { get; set; }
+        
+        [MaxLength(256)]
+        public string Title { get; set; }
+
+        [MaxLength(2048)]
+        public string Body { get; set; }
+
+        [MaxLength(32)]
+        public string email { get; set; }
+
+        public bool Deleted { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+
+        public virtual ICollection<AdvertisementTag> AdvertisementTags { get; set; }
+
+    }
+}
---
+++ src/AdvertBoard/Domain/AdvertBoard.Domain/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AdvertBoard.Domain
+{
+    /// <summary>
+    /// Тег
+    /// </summary>
+    public class Tag
+    {
+        public int Id { get; set; }
+        
+        [MaxLength(32)]
+        public string TagText { get; set; }
+
+        public virtual ICollection<Tag> MyAdvertisements { get; set; }
+    }
+}
